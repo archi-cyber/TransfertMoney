c1b318c316d7483651f2c75b5166d3ec22a2a7c6
junioir <junior.tefo01gmail.com>
2026-05-25 13:49:23 -0400

    improve prismaclient

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  return new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
   });
+  return new PrismaClient({ adapter });
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
